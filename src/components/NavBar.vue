8969fa8267f494dbb635248b69db8bdc789d513d
Qixiny <3185835784@qq.com>
2021-11-29 21:16:21 +0800

    ss

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="nav-bar">
+  </div>
+</template>
+<script lang="ts">
+export default {
+  setup () {
+    return {}
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import '../main.scss';
+#nav-bar  {
+  width: 100%;
+  height: 30px;
+  background-color: $style-color;
+  -webkit-app-region: drag;
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Main View</p>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  setup () {
+    console.log(123)
+    return {}
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
